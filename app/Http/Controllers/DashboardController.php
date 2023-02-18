c82269f010c6f3aac4c3d4f2f8ca272121117682
Rob Wilde <robert@mrwilde.com>
2023-02-18 15:41:34 +1000

    Chapter3: completed the setup

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Site;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function __invoke(Site $site)
+    {
+        return inertia()->render('Dashboard');
+    }
+}
